6729f11ed7a7d1888b17917925c449867eda0c32
Jaclyn Foss <andrew.n.chalmers@gmail.com>
2014-06-17 13:33:27 -0400

    package name change for factories

--- MainActivity/src/factories/EquipmentFactory.java
+++ MainActivity/src/factories/EquipmentFactory.java
@@ -1,29 +1,31 @@
-package Factories;
+package factories;
 
 import java.util.Date;
 
-import models.nodes.Task;
+import models.nodes.Equipment;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class TaskFactory {
-	public static Task build(JSONObject object, String id) {
-	try {
+public class EquipmentFactory {
+
+	public static Equipment build(JSONObject object, String id) {
+		try {
 			String description = object.getString("description");
 			String report_id = object.getString("report_id"); 
+			String part_name = object.getString("part_name"); 
 			String report_index = object.getString("report_index"); 
 			String createID = id;
 			String actualID = object.getString("id");
-			Date completed_at = (Date) object.get("completed_at"); // <--- might fail there 
+			Date scanned_at = (Date) object.get("scanned_at"); // <--- might fail there 
 			String geolocation_id = object.getString("geolocation_id");  
 			String name = actualID + report_id; 
-			String completed = object.getString("completed");
-			Task t = new Task(name, createID, actualID, description, completed_at, report_id, report_index, geolocation_id, completed);
+			Equipment t = new Equipment(name, createID, actualID, description, scanned_at, report_id, report_index, geolocation_id, part_name);
 			return t; 
 		} catch (JSONException e) {
 			e.printStackTrace();
 			return null; 
 		}
 	}
+
 }
