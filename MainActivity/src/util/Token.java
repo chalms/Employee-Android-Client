5ca17acff6f2cfd113bd315fa7e04e5746fe3f74
Jaclyn Foss <andrew.n.chalmers@gmail.com>
2014-06-20 10:04:46 -0400

    Auth Token

---
+++ MainActivity/src/util/Token.java
@@ -0,0 +1,42 @@
+package util;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+class Token {
+	private String token = null;
+	public double ttl = 0;
+	private Integer userId = null;
+	
+
+	boolean isValid() {
+		return ((ttl < 10) || (token == null));
+	}
+
+	public Token(JSONObject theToken) throws JSONException {
+		JSONObject apiSessionToken = theToken.getJSONObject("api_session_token");
+		token = apiSessionToken.getString("token");
+		ttl = apiSessionToken.getDouble("ttl");
+		setUserId(apiSessionToken.getInt("user_id"));
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
+
+}
